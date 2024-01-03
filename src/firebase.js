0e3f705dd5149d244d261009c6769de55df82069
ptera0330 <ptera3303@gmail.com>
2024-01-03 11:45:13 +0900

    '3장'

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apikey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+  };
+
+export default firebase.initializeApp(firebaseConfig)
